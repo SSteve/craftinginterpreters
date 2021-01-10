1bd34df16465add870d8330d31cccf9bf97c5de6
Steve Nicholson <ssteve@mac.com>
2021-01-09 17:05:05 -0800

    Through Chapter 8.

--- java/com/craftinginterpreters/lox/RuntimeError.java
+++ java/com/craftinginterpreters/lox/RuntimeError.java
@@ -1,6 +1,10 @@
 package com.craftinginterpreters.lox;
 
 public class RuntimeError extends RuntimeException {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 2284854715226445885L;
     final Token token;
 
     RuntimeError(Token token, String message) {
